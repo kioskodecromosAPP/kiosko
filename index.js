c7463b9426ff07d8fe235dae8b26c31f9601b6f8
Ivan Sanchez <isancr00@estudiantes.unileon.es>
2021-06-09 13:34:25 +0200

    login compleado falta cerrar connection

--- index.js
+++ index.js
@@ -6,42 +6,53 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const mysql = require('mysql');
-const mainRouter = require('/public/users.js');
 
 
-function comprobarAutentificacion(email, password) {
-    if (email == "123" && password == "root") {
-        return true;
-    } else {
-        return false;
-    }
+ async function comprobarAutentificacion(email, password) {
+    var devuelve = false;
+   await connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("SELECT * FROM USUARIOS WHERE EMAIL=?", [email], async (err, result) => {
+            if (err) throw err;
+
+            //console.log(result.length)
+
+            if(result.length){
+                if (email == result[0].EMAIL && password == result[0].CONTRASENYA) {
+                    console.log("HE ENTRADO");
+                    devuelve = true;
+                   // return devuelve;
+                }
+            }
+        });
+    });
+
+    return devuelve;
+
 }
 
 router.route('/')
-    .post(function(req, res) {
+    .post(function (req, res) {
         console.log(req.body);
 
-        const autentificacion = comprobarAutentificacion(req.body.email, req.body.password);
+        const promise = comprobarAutentificacion(req.body.email, req.body.password);
 
-        if (autentificacion) {
-            res.status(300).send();
+        if (promise) {
+            res.status(200).send();
         } else {
             res.status(400).send();
         }
 
-    })
+    });
 
 app.use('/login', router);
 
-app.use(express.static('public'));
-app.use("/", mainRouter);
 
-app.get('/', function(req, res) {})
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'dbuser',
-    password: 's3kreee7',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '310100sR.',
     database: 'cromos'
 });
 
--- public/script.js
+++ public/script.js
@@ -6,6 +6,10 @@ function cerrarFormularioAcceso(){
     document.getElementById("myForm").style.display = "none";
 }
 
+function registroUsuarios(){
+    window.location.replace("./registro.html");
+}
+
 function login(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("pwd").value;
@@ -17,6 +21,7 @@ function login(){
         },
         body: JSON.stringify({"email":email, "password":password})
     }).then(response =>{
+        console.log(response.status);
         if(response.status == 400){
             alert("El login ha sido incorrecto");
         }else{

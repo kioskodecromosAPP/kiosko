85480b081680130ab9650430a60b608d9b00327a
Ivan Sanchez <isancr00@estudiantes.unileon.es>
2021-06-09 11:13:07 +0200

    Cambios login

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
-
+const router = express.Router();
+const fetch = require('node-fetch');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/',function(req,res){
-})
+function comprobarAutentificacion(email,password){
+    if(email == "123" && password == "root"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+router.route('/')
+    .post(function(req,res){
+       console.log(req.body);
+        
+       const autentificacion = comprobarAutentificacion(req.body.email, req.body.password);
+
+       if(autentificacion){
+           res.status(300).send();
+       }else{
+           res.status(400).send();
+       }
+
+    })
+
+app.use('/login',router);
 
 app.listen(3000,() => {
     console.log("Server on port 3000");
--- public/script.js
+++ public/script.js
@@ -7,5 +7,29 @@ function cerrarFormularioAcceso(){
 }
 
 function login(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pwd").value;
+
+    fetch('/login',{
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({"email":email, "password":password})
+    }).then(response =>{
+        if(response.status == 400){
+            alert("El login ha sido incorrecto");
+        }else{
+            alert("El login es correcto");
+            window.location.replace("./perfil.html");
+        }
+    })
+
+
+
+
+    console.log(email);
+    console.log(password);
+
     
 }

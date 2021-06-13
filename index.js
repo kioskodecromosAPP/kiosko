be87b53fced1697c84aa2516afdabeb21acfa670
Ivan Sanchez <isancr00@estudiantes.unileon.es>
2021-06-13 15:09:42 +0200

    Cambios

--- index.js
+++ index.js
@@ -8,45 +8,84 @@ app.use(express.urlencoded({ extended: true }));
 const mysql = require('mysql');
 
 
- async function comprobarAutentificacion(email, password) {
+async function comprobarAutentificacion(email, password) {
     var devuelve = false;
-   await connection.connect(function (err) {
+    await connection.connect(function(err) {
         if (err) throw err;
-        connection.query("SELECT * FROM USUARIOS WHERE EMAIL=?", [email], async (err, result) => {
+        connection.query("SELECT * FROM USUARIOS WHERE EMAIL=?", [email], async(err, result) => {
             if (err) throw err;
 
             //console.log(result.length)
 
-            if(result.length){
+            if (result.length) {
                 if (email == result[0].EMAIL && password == result[0].CONTRASENYA) {
                     console.log("HE ENTRADO");
                     devuelve = true;
-                   // return devuelve;
+                    // return devuelve;
                 }
             }
         });
     });
-
+    connection.end;
     return devuelve;
 
 }
 
+function comprobarIdUnico(email) {
+    var unico = false;
+    connection.connect(function(err) {
+        if (err) throw err;
+
+        connection.query("SELECT * FROM USUARIOS WHERE EMAIL=?", [email], async(err, result) => {
+            if (err) throw err;
+
+            if (result.length == 0) {
+                unico = true;
+            }
+        })
+    })
+    connection.end;
+    return unico;
+}
+
 router.route('/')
-    .post(function (req, res) {
+    .post(function(req, res) {
         console.log(req.body);
 
         const promise = comprobarAutentificacion(req.body.email, req.body.password);
 
-        if (promise) {
-            res.status(200).send();
-        } else {
-            res.status(400).send();
-        }
+        promise.then(result =>{
+            if (result) {
+                res.status(200).send();
+            } else {
+                res.status(400).send();
+            }
+    
+        })
 
     });
 
 app.use('/login', router);
 
+router.route('/resgitro')
+    .post(function(req, res) {
+        console.log(req.body);
+        //COMPROBAMOS QUE NO EXISTE UN EMAIL IGUAL
+        const aux = comprobarIdUnico(req.body.email);
+        if (aux == false) {
+            alert("Ya hay un usuario registrado con esta dirección de correo");
+        } else {
+            //GUARDAMOS AL USUARIO EN LA BASE DE DATOS
+            connection.connect(function(err) {
+                if (err) throw err;
+                var query = connection.query('INSERT INTO USUARIOS(NOMBRE,APELLIDOS,EMAIL,ESADMIN,CONTRASENYA,PUNTOS) VALUES(?, ?, ?, ?, ?,?)', [req.body.name, req.body.apellidos, req.body.email, '0', req.body.contrasenya, '0'], async(err, result) => {
+                    if (err) throw err;
+                    console.log(query);
+                })
+            })
+            connection.end;
+        }
+    });
 
 
 var connection = mysql.createConnection({
--- public/script.js
+++ public/script.js
@@ -10,6 +10,43 @@ function registroUsuarios(){
     window.location.replace("./registro.html");
 }
 
+function registroUsuarios() {
+    window.location.replace("./registro.html");
+}
+
+function registro() {
+    const name = document.getElementById("nombre").value;
+    const apellidos = document.getElementById("apellidos").value;
+    const email = document.getElementById("em").value;
+    const contrasenya = document.getElementById("cont").value;
+
+    console.log(name);
+    console.log(apellidos);
+    console.log(email);
+    console.log(contrasenya);
+
+    fetch('/registro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "name": name, "apellidos": apellidos, "email": email, "contrasenya": contrasenya })
+    }).then(response => {
+        console.log(response.status);
+        if (response.status == 400) {
+            alert("El registro ha sido incorrecto");
+        } else {
+            alert("El registro es correcto");
+            window.location.replace("./perfil.html");
+        }
+    })
+
+    console.log(name);
+    console.log(apellidos);
+    console.log(email);
+    console.log(contrasenya);
+}
+
 
 function comprobarCookie(){
     
@@ -24,7 +61,6 @@ function comprobarCookie(){
 function login(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("pwd").value;
-    crearCookie();
 
     fetch('/login',{
         method: 'POST',
@@ -33,24 +69,16 @@ function login(){
         },
         body: JSON.stringify({"email":email, "password":password})
     }).then(response =>{
-        console.log(response.status);
         if(response.status == 400){
             alert("El login ha sido incorrecto");
         }else{
             alert("El login es correcto");
+            crearCookie();
             window.location.replace("./perfil.html");
         }
-    })
-
-
-
-
-    console.log(email);
-    console.log(password);
-
-    
+    })    
 }
 
 function crearCookie(){
-    document.cookie = "emailUsuario=" + email + "; max-age=60";
+    document.cookie = "emailUsuario=" + email + "; max-age=1800";
 }
